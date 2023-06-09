98d12cf3e9e573ab4a478f20a27714e74627ece6
codingNode <hasnat.uetian145@gmail.com>
2023-06-09 19:09:44 +0500

    Final changes working

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,7 +6,6 @@ def english_to_french(english_text):
     french_text = MyMemoryTranslator(source='english',target='french').translate(english_text)
     return french_text
 
-
 def french_to_english(french_text):
     """function to translate text from french to english"""
     english_text = MyMemoryTranslator(source='french',target='en').translate(french_text)
